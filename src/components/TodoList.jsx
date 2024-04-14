55f605b1b91af57ecba46767787ecd401f8ca70c
Abdul Majid <majid616365@gmail.com>
2024-04-14 15:49:57 +0530

    init

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,63 @@
+import { useSelector, useDispatch } from "react-redux";
+import {
+  toggleComplete,
+  deleteTodo,
+  addTodo,
+} from "../features/todos/todoSlice";
+import { useState } from "react";
+
+const TodoList = () => {
+  const [newTodoTitle, setNewTodoTitle] = useState("");
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos.todos);
+
+  const handleAddTodo = () => {
+    if (newTodoTitle.trim()) {
+      dispatch(addTodo(newTodoTitle));
+      setNewTodoTitle("");
+    }
+  };
+
+  const handleToggleComplete = (todoId) => {
+    dispatch(toggleComplete(todoId));
+  };
+
+  const handleDeleteTodo = (todoId) => {
+    dispatch(deleteTodo(todoId));
+  };
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <input
+        type="text"
+        value={newTodoTitle}
+        onChange={(e) => setNewTodoTitle(e.target.value)}
+        placeholder="Add a new todo..."
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => handleToggleComplete(todo.id)}
+            />
+            <span
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+                color: todo.completed ? "gray" : "black",
+              }}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
---
+++ src/features/todos/todoSlice.js
@@ -0,0 +1,41 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [],
+};
+
+const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    getTodos: (state, action) => {
+      state.todos = action.payload;
+    },
+    addTodo: {
+      reducer: (state, action) => {
+        state.todos.push(action.payload);
+      },
+      prepare: (text) => ({
+        payload: {
+          id: nanoid(),
+          text,
+          completed: false,
+        },
+      }),
+    },
+    toggleComplete: (state, action) => {
+      const todo = state.todos.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, toggleComplete, deleteTodo, getTodos } =
+  todoSlice.actions;
+
+export default todoSlice.reducer;
